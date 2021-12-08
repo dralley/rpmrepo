68ae6a1ec1e845bdd766e2f2d6686922fa3d8b95
Daniel Alley <dalley@redhat.com>
2021-12-08 11:51:38 -0500

    Update manifest to includes requirements files

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("requirements.txt") as requirements:
     requirements = requirements.readlines()
 
 setup(
     name="rpmrepo",
-    version="0.1.0.dev",
+    version="0.1.0.dev1",
     description='A library and CLI tool providing facilities for working with RPM repositories.',
     author='Daniel Alley',
     license='GPLv2',
     install_requires=requirements,
     packages=["rpmrepo"],
-    # py_modules=['rpmrepo'],
     entry_points={
         'console_scripts': [
             'rpmrepo=rpmrepo.cli:cli',
