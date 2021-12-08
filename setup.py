cbec2afd0212fa12da8a04800feaf6deafc62e12
Daniel Alley <dalley@redhat.com>
2021-12-08 12:13:13 -0500

    Fix packaging (again)

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("requirements.txt") as requirements:
     requirements = requirements.readlines()
 
 setup(
     name="rpmrepo",
-    version="0.1.0.dev1",
+    version="0.1.0.dev2",
     description='A library and CLI tool providing facilities for working with RPM repositories.',
     author='Daniel Alley',
     license='GPLv2',
     install_requires=requirements,
-    packages=["rpmrepo"],
+    packages=find_packages(exclude=["test"]),
     entry_points={
         'console_scripts': [
             'rpmrepo=rpmrepo.cli:cli',
